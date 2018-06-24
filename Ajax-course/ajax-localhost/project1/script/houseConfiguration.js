32b76ce69be7ec35c05a86795c99dc9a38f83db5
Sasha <oleksanova@gmail.com>
2018-06-24 10:47:13 -0500

    Adding project1 from and start on form validation.

---
+++ Ajax-course/ajax-localhost/project1/script/houseConfiguration.js
@@ -0,0 +1,154 @@
+
+var errorMessage = '';
+
+function displayEstimate() {
+
+    var errorMessage = document.getElementsByClassName('message');
+
+
+    for ( var index = 0; index < errorMessage.length; index ++ ) {
+
+        console.log(errorMessage[index]);
+
+        if ( errorMessage[index] ) {
+            errorMessage[index].parentNode.removeChild(errorMessage[index]);
+        }
+    }
+
+    houseConfigurationData();
+    
+}
+
+function houseConfigurationData() {
+
+    var houseType = document.getElementById('hoseType');
+    var houseTypeUserInput = houseType.options[houseType.selectedIndex].value;
+
+    var color = document.getElementById('color');
+    var colorUserInput = color.options[color.selectedIndex].value;
+
+    var vehicles = document.getElementsByName('vehicles');
+    var vehiclesUserInput = '';
+
+    for ( var index = 0; index < vehicles.length; index ++ ) {
+
+        if ( vehicles[index].checked ) {
+
+            vehiclesUserInput = vehicles[index].value;
+        }
+    }
+
+    var materialType = document.getElementById('materialType');
+    var materialTypeUserInput = materialType.options[materialType.selectedIndex].value;
+
+    var sqFootageUserInputString = document.getElementById('sqFootage').value;
+    var sqFootageUserInputInt = parseInt(sqFootageUserInputString);
+
+
+    validateHouseTypeInput(houseTypeUserInput);
+    validateColorUserInput(colorUserInput);
+    validateMaterialTypeUserInput(materialTypeUserInput);
+    validateFootageUserInput(sqFootageUserInputString);
+
+
+}
+
+
+function validateHouseTypeInput(houseTypeUserInput) {
+
+    errorMessage = 'Please enter house type';
+
+    if ( houseTypeUserInput.length == 0 ) {
+
+        getErrorMessage();
+
+    } else {
+
+        console.log(houseTypeUserInput);
+    }
+
+}
+
+function validateColorUserInput(colorUserInput) {
+
+    errorMessage = 'Please enter color';
+
+    if ( colorUserInput.length == 0 ) {
+
+        getErrorMessage();
+
+    } else {
+
+        console.log(colorUserInput);
+    }
+
+}
+
+function validateMaterialTypeUserInput(materialTypeUserInput) {
+
+    errorMessage = 'Please enter material type';
+
+    if ( materialTypeUserInput.length == 0 ) {
+
+        getErrorMessage();
+
+    } else {
+
+        console.log(materialTypeUserInput);
+    }
+}
+
+function validateFootageUserInput(sqFootageUserInputString) {
+
+    errorMessage = 'Please enter square footage';
+
+    if ( sqFootageUserInputString.trim().length == 0 || sqFootageUserInputString.trim() == '') {
+
+        getErrorMessage();
+
+    } else {
+        console.log(sqFootageUserInputString);
+    }
+}
+
+
+function getErrorMessage() {
+
+    var h4 = document.createElement('h4');
+    h4.className = 'message';
+
+    var messageTextNode = document.createTextNode(errorMessage);
+    document.body.appendChild(h4);
+
+    h4.appendChild(messageTextNode);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
