8f91270597f7e51fd972770114a7196311a8065d
Sasha <oleksanova@gmail.com>
2018-07-25 23:08:54 -0500

    Adding display all tasks project3.

--- Ajax-course/ajax-localhost/project3/script/project3.js
+++ Ajax-course/ajax-localhost/project3/script/project3.js
@@ -1,9 +1,34 @@
 $(document).ready(function () {
-    console.log('test');
 
     var url = 'ToDo/allTasks.php';
 
-    $.get(url, function (response) {
-        console.log(response);
+    $('<table id="table"></table>').appendTo('body');
+
+    $.get(url, function(response) {
+
+        $(response).find('task').each(function () {
+
+            var taskId = $(this).find('id').text();
+            var description = $(this).find('description').text();
+            var deleteButton = $('<button type="submit" id="delete"><i id="trashIcon" class="fa fa-trash"></i></button>');
+
+
+             $('#table').append('<tr>');
+             $('tr').last().append('<td>');
+
+             $('td').last().append(deleteButton);
+             $('td').last().append(description);
+
+            $('button').on('click', function () {
+                $(this).parents('tr').remove();
+
+            });
+        });
+        
     }, 'xml');
+
+
+
+
+
 });
